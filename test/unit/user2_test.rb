c3dd9865ec39e51e75c56270d5dc7e2a56f4c808
Kir Maximov <kir@maxkir.com>
2012-02-14 15:45:57 +0100

    rename file

---
+++ test/unit/user2_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "the truth" do
+     assert true
+  end
+
+  test "the truth2" do
+     assert false
+  end
+  test "the truth3" do
+     assert false
+  end
+  test "the truth4" do
+     assert false
+  end
+
+  def test_another
+    assert false
+  end
+end
